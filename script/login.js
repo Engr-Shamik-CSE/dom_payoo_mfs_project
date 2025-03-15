426abb26d03d38ffbdb592914c0cd1411f5e061b
unknown <you@example.com>
2025-03-15 20:51:56 +0600

    3 steps done

---
+++ script/login.js
@@ -0,0 +1,21 @@
+//login button call
+const loginBtn = document.getElementById('btn-login');
+loginBtn.addEventListener('click',
+    function (event) {
+        // to stop the default refreshing problem of the form
+        event.preventDefault();
+        const accountNumber = document.getElementById('account-number').value;
+        const pinNumber = document.getElementById('pin-number').value;
+        const convertedPIN = parseInt(pinNumber);//convert string to integer
+
+        if (accountNumber.length === 11) {
+            if (convertedPIN === 1234) {
+                console.log('PIN thik ache');
+                window.location.href="./main.html";// redirecting to HomePage
+            } else {
+                console.log('PIN thik nai')
+            }
+        }else{
+            console.log('Give valid Phone Number');
+        }
+    });
